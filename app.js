a07d8b559c1ea985e6183265a5af917e729680a5
tmkelly28 <tmkelly28@gmail.com>
2015-10-06 18:12:09 -0400

    Added initial views and routes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -17,8 +16,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', swig.renderFile)
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -66,6 +63,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(8080, () => console.log("Express server listening on PORT 8080")});
+app.listen(8080, function () {
+  console.log("Express server listening on PORT 8080");
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var models = require('../models');
+
+var Hotel = models.Hotel;
+var Restaurant = models.Restaurant;
+var Activity = models.Activity;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var data = {};
+
+	Hotel.find({}).exec()
+	.then(function(hotels) {
+		return data['hotels'] = hotels;
+	})
+	.then(function() {
+		return Restaurant.find({}).exec();
+	})
+	.then(function(restaurants) {
+		return data['restaurants'] = restaurants;
+	})
+	.then(function() {
+		return Activity.find({}).exec();
+	})
+	.then(function(activities) {
+		data['activities'] = activities;
+		return data
+	})
+	.then(function(data) {
+		res.locals = data;
+		res.render('index');
+	})
+	.catch(function(err) {
+		console.log(err);
+	});
 });
 
 module.exports = router;
